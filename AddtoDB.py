18f26ffc909cc3572092d792945d8d63f49f5bcb
Abby Sininger <143823566+abby-973@users.noreply.github.com>
2025-03-04 02:41:25 +0000

    working add to 3db

--- AddtoDB.py
+++ AddtoDB.py
@@ -1,7 +1,52 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+# Ensure templates directory exists
+if not os.path.exists('templates'):
+    os.makedirs('templates')
+
+# Create a default layout.html file if it doesn't exist
+layout_template_path = os.path.join('templates', 'layout.html')
+if not os.path.exists(layout_template_path):
+    with open(layout_template_path, 'w') as f:
+        f.write("""<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>{% block title %}{% endblock %}</title>
+        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='site.css')}}" />
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="{{ url_for('home') }}" class="navbar-brand">Home</a>
+            <a href="{{ url_for('register') }}" class="navbar-item">Register</a>
+            <a href="{{ url_for('users') }}" class="navbar-item">Add Users</a>
+            <a href="{{ url_for('addpin') }}" class="navbar-item">Add Pin</a>
+            <a href="{{ url_for('addboard') }}" class="navbar-item">Create Board</a>
+        </div>
+        <div class="body-content">
+            {% block content %}
+            {% endblock %}
+            <hr/>
+            <footer>
+                <p>Pinspire</p>
+            </footer>
+        </div>
+    </body>
+</html>""")
+
+# Create a default home.html file if it doesn't exist
+home_template_path = os.path.join('templates', 'home.html')
+if not os.path.exists(home_template_path):
+    with open(home_template_path, 'w') as f:
+        f.write("""{% extends 'layout.html' %}
+{% block title %}Home{% endblock %}
+{% block content %}
+<h1>Welcome to Pinspire add to db!</h1>
+{% endblock %}""")
 
 # Home Page route
 @app.route("/")
@@ -48,8 +93,12 @@ def users():
     return render_template("users.html", rows=rows)
 
 # Route to add a new pin
-@app.route("/addpin", methods=['POST'])
+@app.route("/addpin")
 def addpin():
+    return render_template("addpin.html")
+
+@app.route("/insertpin", methods=['POST'])
+def insertpin():
     if request.method == 'POST':
         try:
             image = request.form['image']
@@ -70,8 +119,12 @@ def addpin():
             return render_template('result.html', msg=msg)
 
 # Route to create a board
-@app.route("/addboard", methods=['POST'])
+@app.route("/addboard")
 def addboard():
+    return render_template("addboard.html")
+
+@app.route("/insertboard", methods=['POST'])
+def insertboard():
     if request.method == 'POST':
         try:
             cover_image = request.form['cover_image']
@@ -86,49 +139,6 @@ def addboard():
         except:
             con.rollback()
             msg = "Error in creating board"
-        finally:
-            con.close()
-            return render_template('result.html', msg=msg)
-
-# Route to add a pin to a board
-@app.route("/addpinboard", methods=['POST'])
-def addpinboard():
-    if request.method == 'POST':
-        try:
-            board_id = request.form['board_id']
-            pin_id = request.form['pin_id']
-
-            with sqlite3.connect('database.db') as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO boardPins (board_id, pin_id) VALUES (?, ?)",
-                            (board_id, pin_id))
-                con.commit()
-                msg = "Pin successfully added to board"
-        except:
-            con.rollback()
-            msg = "Error in adding pin to board"
-        finally:
-            con.close()
-            return render_template('result.html', msg=msg)
-
-# Route to add a comment to a pin
-@app.route("/addcomment", methods=['POST'])
-def addcomment():
-    if request.method == 'POST':
-        try:
-            text = request.form['text']
-            pin_id = request.form['pin_id']
-            user_id = request.form['user_id']
-
-            with sqlite3.connect('database.db') as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO comments (text, pin_id, user_id) VALUES (?, ?, ?)",
-                            (text, pin_id, user_id))
-                con.commit()
-                msg = "Comment successfully added"
-        except:
-            con.rollback()
-            msg = "Error in adding comment"
         finally:
             con.close()
             return render_template('result.html', msg=msg)
